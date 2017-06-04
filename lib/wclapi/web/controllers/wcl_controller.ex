975e4254fd521f16cbc0858b10b58042b0bd2bb5
Matthew Neel <matt@emeraldkingdomgreenhouse.com>
2017-06-04 06:27:21 -0700

    Small syntax fix.

--- lib/wclapi/web/controllers/wcl_controller.ex
+++ lib/wclapi/web/controllers/wcl_controller.ex
@@ -2,6 +2,6 @@ defmodule Wclapi.Web.WclController do
     use Wclapi.Web, :controller
 
     def loans(conn, %{"pid" => pid, "surname" => surname} = _params) do
-        conn |> json WclEx.loans(pid, surname)
+        conn |> json(WclEx.loans(pid, surname))
     end
 end
